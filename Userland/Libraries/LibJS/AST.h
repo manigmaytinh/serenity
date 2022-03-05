manigmaytinh/serenity

commit 72689ce7bd8b4668ad5afd0777e96668c5044eb7

LibJS: Add fast_is<PrivateIdentifier>()

Userland/Libraries/LibJS/AST.h
@@ -66,6 +66,7 @@ public:
     virtual bool is_class_expression() const { return false; }
     virtual bool is_expression_statement() const { return false; }
     virtual bool is_identifier() const { return false; }
+    virtual bool is_private_identifier() const { return false; }
     virtual bool is_scope_node() const { return false; }
     virtual bool is_program() const { return false; }
     virtual bool is_class_declaration() const { return false; }
@@ -2088,6 +2089,9 @@ inline bool ASTNode::fast_is<ClassExpression>() const { return is_class_expressi
 
 template<>
 inline bool ASTNode::fast_is<Identifier>() const { return is_identifier(); }
+
+template<>
+inline bool ASTNode::fast_is<PrivateIdentifier>() const { return is_private_identifier(); }
 
 template<>
 inline bool ASTNode::fast_is<ExpressionStatement>() const { return is_expression_statement(); }

commit 10c870b82876f5f97ce478a83491d1708c765a14

LibJS: Actually override is_private_identifier() in PrivateIdentifier
Regression from 72689ce7bd8b4668ad5afd0777e96668c5044eb7.

Userland/Libraries/LibJS/AST.h
@@ -1220,6 +1220,8 @@ public:
     virtual Completion execute(Interpreter&, GlobalObject&) const override;
     virtual void dump(int indent) const override;
 
+    virtual bool is_private_identifier() const override { return true; }
+
 private:
     FlyString m_string;
 };